f2690a74cafa6435a99216ef23f13185d415b2ce
Minahil Nawaz <minahilnawaz98@gmail.com>
2025-04-18 23:41:50 +0500

    add TS and JS

---
+++ Minhail Nawaz/JS&TS/paractice/Datatypes/app.js
@@ -0,0 +1,27 @@
+var FirstName = "John";
+console.log(FirstName);
+// Number
+// any number -2,or 2.0 or simple have data type number
+// String
+// Anything come inside double quotes is a string
+// Boolean
+// Null
+// Undefined
+// Symbol
+// BigInt
+let Age: Number = -25.4;
+console.log(Age);
+// to check the type of variable
+console.log(typeof Age); // number
+// String
+let Name: String = "John Doe";
+console.log(Name);
+console.log(typeof Name); // string
+// Boolean
+let isMarried: boolean = false;
+console.log(isMarried);
+console.log(typeof isMarried); // boolean
+// undefined
+var myVar = ;
+console.log(myVar);
+console.log(typeof myVar); // undefined
---
+++ Minhail Nawaz/JS&TS/paractice/variables/app.js
@@ -0,0 +1,22 @@
+var FirstName = "John";
+console.log(FirstName);
+// let age: number = 25;
+// console.log(age);
+// declaration 
+// 1. declare a variable
+var age;
+// initialization
+// 2. initialize a variable
+age = "25";
+console.log(age);
+// RULES for NAMING VARIABLES
+// 1. variable name should  not start with a letter, underscore or dollar sign
+// let 3age: Number = 25; // invalid
+// 2. variable name should not contain special characters except underscore and dollar sign
+// let ag#e: Number = 25; // invalid
+// 3. variable name should not have spaces
+// let first name: string = "John"; // invalid
+// 4. variable name should not be a reserved keyword
+// let let: string = "John"; // invalid
+// sometime they can be  used as variable name but not recommended
+var name = "John"; // valid but not recommended
