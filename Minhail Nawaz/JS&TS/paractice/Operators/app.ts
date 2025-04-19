a4fa0543a856cafe22bde3258b1a300118aff5a3
Minahil Nawaz <minahilnawaz98@gmail.com>
2025-04-19 23:11:31 +0500

    add Operators and Tamplate literals

---
+++ Minhail Nawaz/JS&TS/paractice/Operators/app.ts
@@ -0,0 +1,47 @@
+// Math operators
+let CurrentYear: number = 2025;
+let AgeCalculation: number =CurrentYear - 2005;
+console.log(AgeCalculation); // 20
+
+console.log(10 + 5); // 2030
+console.log(10 *5); // 2025
+console.log(10 ** 5); // 2020
+
+
+let x: number = 10;
+// x++ ; // whatever the values plus 1 in that value
+console.log(x); // 11
+// x-- ; // whatever the values minus 1 in that value
+console.log(x); // 10
+
+// // for multiply
+x *= 2;
+
+console.log(x); 
+x +=5
+console.log(x); 
+x -= 10;
+console.log(x); // 15
+
+x /=3;
+console.log(x); // 3.3333333333333335
+
+// for getting that we have to write the code outside the print statement
+// for getting it in the print statement we have to write it that way
+console.log(x++); //  remains the same
+console.log(++x); //  plus 1
+
+
+// COMPARISON OPERATORS
+
+let a: number = 5;
+let b: number = 10;
+let c: boolean = a<b;
+console.log(c); // true
+
+let d: boolean = CurrentYear - 2004 > CurrentYear-1047;
+console.log(d); // true
+
+let e = x = 25 - 10 -5;
+console.log(e , x); // 10
+
